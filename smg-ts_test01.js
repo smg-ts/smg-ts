37fbbdad7b1fcb91f81d24b4d8a1cb8804f75ac3
smg-ts <84409007+smg-ts@users.noreply.github.com>
2021-05-19 01:32:53 +0300

    Add files via upload

---
+++ smg-ts_test01.js
@@ -0,0 +1,19 @@
+const { Browser, By, Key, until } = require('selenium-webdriver')
+const { ignore, suite } = require('selenium-webdriver/testing')
+
+suite(function (env) {
+  describe('Google Search', function () {
+    let driver
+
+    before(async function () {
+      driver = await env.builder().build()
+    })
+
+    it('demo', async function () {
+      await driver.get('https://www.google.com/ncr')
+      await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
+      await driver.wait(until.titleIs('webdriver - Google Search'), 1000)
+    })
+    after(() => driver && driver.quit())
+  })
+})
